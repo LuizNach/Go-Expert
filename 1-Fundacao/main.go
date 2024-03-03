d747341e43dc85e4efb36e4dc4250143f4cd7444
LuizNach <11381501+LuizNach@users.noreply.github.com>
2024-03-03 19:22:54 -0300

    chore: 1-Fundação package rules comments

--- 1-Fundacao/main.go
+++ 1-Fundacao/main.go
@@ -1,5 +1,13 @@
 package main
 
+/**
+Primeira lição:
+- Todo arquivo deve referencia um pacote em seu inicio
+- Todo nome de pacote deve ser do mesmo nome do folder/directory que contem os arquivos go
+- Único arquivo que não possui o nome do pacote como nome do directory/folder é o main.go
+- Quando há mais de um arquivo .go no mesmo folder/directry, todo devem ter o mesmo nome de pacote
+*/
+
 func main() {
 	println("Hello, world!")
 }
