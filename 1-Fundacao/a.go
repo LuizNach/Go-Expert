615d8ac0c909887b89f0841b3b9abf48be8bb490
LuizNach <11381501+LuizNach@users.noreply.github.com>
2024-03-03 19:51:40 -0300

    chore: 1-Fundação execute multi go files

---
+++ 1-Fundacao/a.go
@@ -0,0 +1,3 @@
+package main
+
+const a = "Uma string nova para teste"
--- 1-Fundacao/main.go
+++ 1-Fundacao/main.go
@@ -6,8 +6,9 @@ Primeira lição:
 - Todo nome de pacote deve ser do mesmo nome do folder/directory que contem os arquivos go
 - Único arquivo que não possui o nome do pacote como nome do directory/folder é o main.go
 - Quando há mais de um arquivo .go no mesmo folder/directry, todo devem ter o mesmo nome de pacote
+- Quando há dois arquivos .go no mesmo folder com mesmo package name, quer dizer que tudo que há em um arquivo há no outro também se rodarmos "go run *"
 */
 
 func main() {
-	println("Hello, world!")
+	println(a)
 }
