c5920e88ac27e2f09fc6a4ee2bd996fcd1862da8
LuizNach <11381501+LuizNach@users.noreply.github.com>
2024-03-04 00:02:51 -0300

    chore: 1-Entendendo a primeira linha

---
+++ 1-Fundacao/1-Entendendo a primeira linha/main.go
@@ -0,0 +1,14 @@
+package main
+
+/**
+Primeira lição:
+- Todo arquivo deve referencia um pacote em seu inicio
+- Todo nome de pacote deve ser do mesmo nome do folder/directory que contem os arquivos go
+- Único arquivo que não possui o nome do pacote como nome do directory/folder é o main.go
+- Quando há mais de um arquivo .go no mesmo folder/directry, todo devem ter o mesmo nome de pacote
+- Quando há dois arquivos .go no mesmo folder com mesmo package name, quer dizer que tudo que há em um arquivo há no outro também se rodarmos "go run *"
+*/
+
+func main() {
+	println(a)
+}
