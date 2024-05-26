51237befc90f8d01453117a701816a54af589e71
LuizNach <11381501+LuizNach@users.noreply.github.com>
2024-05-26 01:42:13 -0300

    chore: 2-Preparando base de codigo

---
+++ 4-Banco de Dados/2-Preparando base de código/go.mod
@@ -0,0 +1,5 @@
+module github.com/luiz.nach/banco-de-dados
+
+go 1.21.10
+
+require github.com/google/uuid v1.6.0
---
+++ 4-Banco de Dados/2-Preparando base de código/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+func NewProduct(name string, price float64) *Product {
+	return &Product{
+		ID:    uuid.New().String(),
+		Name:  name,
+		Price: price,
+	}
+}
+
+func main() {
+	fmt.Printf("Hello there\n")
+
+}
