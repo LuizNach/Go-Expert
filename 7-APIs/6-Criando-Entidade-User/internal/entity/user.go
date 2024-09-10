09d70add1ffda4ff20a5d808b56b25f519049b04
LuizNach <11381501+LuizNach@users.noreply.github.com>
2024-09-10 15:25:28 -0300

    chore: Add User entity, add bcrypt dep

---
+++ 7-APIs/6-Criando-Entidade-User/internal/entity/user.go
@@ -0,0 +1,36 @@
+package entity
+
+import (
+	"log"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+// VO - Value Object
+type User struct {
+	ID       string `json:"id"`
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Password string `json:"-"` // MARK Não entendi bem essa parte
+}
+
+func NewUser(name string, email string, password string) (*User, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+	return &User{
+		ID:       "id",
+		Name:     name,
+		Email:    email,
+		Password: string(hash), // Nunca guardamos a senha de forma pura, sempre incriptamos para guardar nodb
+	}, nil
+}
+
+func (u *User) ValidatePassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password)) // Se bater tudo certo, retornará nil pois não ha erros
+	if err != nil {
+		log.Printf("Error on checking bcrypt hash validation on user: %s", u.ID)
+	}
+	return err != nil
+}
